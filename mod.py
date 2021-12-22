e7510a0622b69777c814e7b64b16df676880a674
Shane Liesegang <shane@techie.net>
2021-12-22 07:55:59 -0500

    whoops

--- mod.py
+++ mod.py
@@ -30,6 +30,10 @@ for buildscript in scripts:
     script_content = open(f"./script/{buildscript}", "r").read()
     script_content = script_content.replace("Fira Code", "Fira Mod")
     script_content = script_content.replace("FiraCode", "FiraMod")
+    script_file = open(f"./script/buildscript", "w")
+    script_file.write(script_content)
+    script_file.close()
+
 
 with open(GLYPHS_FILE_OUT, "w") as outfile:
     outfile.write("\n".join(glyphs_lines))
