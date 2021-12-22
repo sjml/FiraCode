78c14f8802f6cf8cc5e5158ceab90d03a5177e48
Shane Liesegang <shane@techie.net>
2021-12-22 07:40:04 -0500

    restoring upstream base along with modification code

--- mod.py
+++ mod.py
@@ -1,21 +1,26 @@
-import sys
+import sys, os
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 GLYPHS_FILE_IN = "./FiraCode.glyphs"
 GLYPHS_FILE_OUT = "./FiraMod.glyphs"
 if len(sys.argv) > 1:
     GLYPHS_FILE_OUT = sys.argv[1]
 
-##### Not actually doing anything, just testing the workflow
 
-# glyphs_content = open(GLYPHS_FILE_IN, "r").read()
+glyphs_content = open(GLYPHS_FILE_IN, "r").read()
 
 # # glyphs_content = glyphs_content.replace('familyName = "Fira Code";', 'familyName = "Fira Mod";')
 
-# # glyphs_lines = glyphs_content.splitlines()
-# # calt_idx = glyphs_lines.index("name = calt;")
-# # glyphs_lines[calt_idx-1] = 'code = "";'
+## I really want to disable all the contextual alternates, but leaving the code entry blank
+##   or removing this section entirely makes the compilation fail. I don't understand the
+##   file format well enough to be more surgical about this, so I'm setting up a single dummy
+##   subsitution rule that has no actual effect.
+glyphs_lines = glyphs_content.splitlines()
+calt_idx = glyphs_lines.index("name = calt;")
+glyphs_lines[calt_idx-1] = 'code = "sub zero by zero;";'
 
-# with open(GLYPHS_FILE_OUT, "w") as outfile:
-#     outfile.write("\n".join(glyphs_lines))
+with open(GLYPHS_FILE_OUT, "w") as outfile:
+    outfile.write("\n".join(glyphs_lines))
 
 
